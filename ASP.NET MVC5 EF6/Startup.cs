890617254af29f2de533ad12f6e5c13723dc7f85
kevin <zdt_kevin@qq.com>
2016-05-22 15:27:40 +0800

    首次修改

---
+++ ASP.NET MVC5 EF6/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5_EF6.Startup))]
+namespace ASP.NET_MVC5_EF6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
+
+
+//测试需改。 
